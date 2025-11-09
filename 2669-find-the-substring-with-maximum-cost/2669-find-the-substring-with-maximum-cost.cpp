ac92eca70e1beb2b96cf546afe6fe5992ceb8366
Rut-Vahab <rt80009000@gmail.com>
2025-11-09 14:34:06 +0200

    Time: 4 ms (74.25%), Space: 37.5 MB (86.7%) - LeetHub

---
+++ 2669-find-the-substring-with-maximum-cost/2669-find-the-substring-with-maximum-cost.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // int maximumCostSubstring(string s, string chars, vector<int>& vals) {
+int maximumCostSubstring(string s, string chars, vector<int>& vals) {
+    // מערך ערכים לכל אות באנגלית
+    vector<int> value(26);
+    for (int i = 0; i < 26; i++) value[i] = i + 1;
+    for (int i = 0; i < chars.size(); i++) {
+        value[chars[i] - 'a'] = vals[i];
+    }
+
+    int max_sum = 0;
+    int current_sum = 0;
+
+    for (char c : s) {
+        current_sum += value[c - 'a'];
+        if (current_sum < 0) current_sum = 0;
+        max_sum = max(max_sum, current_sum);
+    }
+
+    return max_sum;
+}
+
+    // }
+};
