964875f2fcbe2319417d30ee6f2bf3dc3ebba338
Rut-Vahab <rt80009000@gmail.com>
2026-02-11 16:41:53 +0200

    Time: 0 ms (100%), Space: 19.4 MB (28.11%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,39 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        negative = False 
+        num = 0 
+        s = s.lstrip()
+        for i, char  in enumerate(s):
+            if s[i] ==  "-" and i == 0 :
+                negative = True
+            elif s[i] == "+" and i == 0:  # טיפול בסימן +
+                continue
+                print("dd")
+            elif s[i].isdigit() :
+                print (char)
+                if char == "0" and num == 0 :
+                    continue
+                num = num*10
+                num += int(char)
+            else : 
+                break
+      
+        if negative :
+            num = num*-1
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+        if num > INT_MAX:
+            return INT_MAX
+        if num < INT_MIN:
+            return INT_MIN
+
+        return num
+
+        
+        return num
+
+
+       
+
+
+        
